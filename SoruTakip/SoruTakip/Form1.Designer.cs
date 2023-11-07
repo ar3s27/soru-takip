9df235faf62fc4ba15719c1302841b673846b45a
Veysel <veyselerkoyuncu27@gmail.com>
2023-11-07 22:52:23 +0300

    l

---
+++ SoruTakip/SoruTakip/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SoruTakip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.matTxt = new System.Windows.Forms.TextBox();
+            this.geoTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.turTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Right;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(414, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Empty,
+        System.Drawing.Color.Empty,
+        System.Drawing.Color.Empty};
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Ders";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(526, 526);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // matTxt
+            // 
+            this.matTxt.Location = new System.Drawing.Point(142, 64);
+            this.matTxt.Name = "matTxt";
+            this.matTxt.Size = new System.Drawing.Size(100, 20);
+            this.matTxt.TabIndex = 2;
+            // 
+            // geoTxt
+            // 
+            this.geoTxt.Location = new System.Drawing.Point(142, 107);
+            this.geoTxt.Name = "geoTxt";
+            this.geoTxt.Size = new System.Drawing.Size(100, 20);
+            this.geoTxt.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(27, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Geometri:";
+            // 
+            // turTxt
+            // 
+            this.turTxt.Location = new System.Drawing.Point(142, 151);
+            this.turTxt.Name = "turTxt";
+            this.turTxt.Size = new System.Drawing.Size(100, 20);
+            this.turTxt.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(51, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Türkçe:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Matematik:";
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(151, 205);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(75, 77);
+            this.BtnUpdate.TabIndex = 8;
+            this.BtnUpdate.Text = "Güncelle";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 526);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.turTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.geoTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.matTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox matTxt;
+        private System.Windows.Forms.TextBox geoTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox turTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnUpdate;
+    }
+}
+
---
+++ SoruTakip/SoruTakip/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SoruTakip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=AFRODIT\SQLEXPRESS;Initial Catalog=SoruTakip;Integrated Security=True");
+
+        private int GetCurrentDatabaseValue(string ders)
+        {
+            int value = 0;
+            baglanti.Open();
+            SqlCommand selectCommand = new SqlCommand("SELECT CozulenSoru FROM DersSoru WHERE Dersler = @ders", baglanti);
+            selectCommand.Parameters.AddWithValue("@ders", ders);
+            SqlDataReader reader = selectCommand.ExecuteReader();
+            if (reader.Read())
+            {
+                value = reader.GetInt32(0);
+            }
+            baglanti.Close();
+            return value;
+        }
+
+        private void UpdateDatabase(string ders, int value)
+        {
+            baglanti.Open();
+
+            // Update the database using parameterized queries
+            SqlCommand updateCommand = new SqlCommand("UPDATE DersSoru SET CozulenSoru = @value WHERE Dersler = @ders", baglanti);
+            updateCommand.Parameters.AddWithValue("@value", value);
+            updateCommand.Parameters.AddWithValue("@ders", ders);
+            updateCommand.ExecuteNonQuery();
+
+            baglanti.Close();
+        }
+
+
+
+        private void RefreshChart()
+        {
+            chart1.Series["Ders"].Points.Clear(); // Clear existing data
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select Dersler, CozulenSoru from DersSoru", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                chart1.Series["Ders"].Points.AddXY(oku[0].ToString(), oku[1].ToString());
+            }
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select Dersler,CozulenSoru from DersSoru", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                chart1.Series["Ders"].Points.AddXY(oku[0].ToString(), oku[1].ToString());
+            }
+            baglanti.Close();
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            // Get the values from textboxes
+            int matValue = int.Parse(matTxt.Text);
+            int geoValue = int.Parse(geoTxt.Text);
+            int turValue = int.Parse(turTxt.Text);
+
+            // Get the current values from the database
+            int currentMatValue = GetCurrentDatabaseValue("Matematik");
+            int currentGeoValue = GetCurrentDatabaseValue("Geometri");
+            int currentTurValue = GetCurrentDatabaseValue("Turkce");
+
+            // Calculate the new values by adding the entered values to the current values
+            int updatedMatValue = currentMatValue + matValue;
+            int updatedGeoValue = currentGeoValue + geoValue;
+            int updatedTurValue = currentTurValue + turValue;
+
+            // Update the database with the new values
+            UpdateDatabase("Matematik", updatedMatValue);
+            UpdateDatabase("Geometri", updatedGeoValue);
+            UpdateDatabase("Turkce", updatedTurValue);
+
+            // Refresh the chart
+            RefreshChart();
+        }
+    }
+}
